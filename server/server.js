c038dabdf2fa75643b03d453bbcebc78a93692cd
Bryan Bickel <briimcfly@gmail.com>
2023-11-14 11:41:54 -0800

    comment bombed server.js

--- server/server.js
+++ server/server.js
@@ -4,41 +4,49 @@ const {ApolloServer} = require('@apollo/server');
 const {expressMiddleware} = require('@apollo/server/express4');
 const path = require('path');
 
-//GraphQL TypeDefs and Resolvers
+//GraphQL TypeDefs and Resolvers dependencies
 const {typeDefs, resolvers} = require('./schemas');
 
-//Database Connection 
+//Database configuration
 const db = require('./config/connection');
 
+//Server Port: Defaults to 3001 if not set in env variable
 const PORT = process.env.PORT || 3001;
 
+//Initialize an Express App
 const app = express();
 
+//Initialize an Apollo Server with GraphQL Schema 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 })
 
-//New Apollo Server with GraphQL Schema 
+//Function to start the Apollo Server & Set Middleware
 const startApolloServer = async() => {
+    //Start Apollo Server
     await server.start();
 
+    //Parse URL-encoded data and JSON data
     app.use(express.urlencoded({extended: false}));
     app.use(express.json());
 
+    //Apollo's middleware for 'graphql' endpoint
     app.use('/graphql', expressMiddleware(server, {
         // context: authMiddleware
     }));
 
-    //Production Distrubution 
+    //Serve static files in production from dist folder
     if(process.env.NODE_ENV === 'production') {
         app.use(express.static(path.join(__dirname, '../client/dist')));
 
+        // Redirect all requests to the client's 'index.html' in production
         app.get('*', (req,res) => {
             res.sendFile(path.join(__dirname, '../client/dist/index.html'));
         })
     }
 
+    //Connect to DB and start Express Server
     db.once('open', () => {
         app.listen(PORT, () => {
             console.log(`Good Merning! Server is listening on port ${PORT}!`);
