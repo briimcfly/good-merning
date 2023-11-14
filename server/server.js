5e7a4ca138910848a8a709a05bc5206d2ef00a8d
Bryan Bickel <briimcfly@gmail.com>
2023-11-14 11:13:58 -0800

    added typeDefs and resolver files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,20 @@
+//Dependencies
 const express = require('express');
 const {ApolloServer} = require('@apollo/server');
 const {expressMiddleware} = require('@apollo/server/express4');
 const path = require('path');
 
+//GraphQL TypeDefs and Resolvers
+const {typeDefs, resolvers} = require('./schemas');
+
+//Database Connection 
 const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
