c65ef3ae594b4813c2f0039295efaa971e353c5f
Bryan Bickel <briimcfly@gmail.com>
2023-11-14 15:56:28 -0800

    added user TypeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,35 @@
+//GraphQL type definitions 
+
 const typeDefs = `
-    # Simple root Query to get servers started... 
+    # User: Id, Username, Email, & Password 
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+    }
+
+    # Authentication: Includes token and user object
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    # Root Query/Endpoint 
     type Query {
-        hello: String
+        # Return an array of user types 
+        users: [User]
+        # Returns a single user based on provided username 
+        user(username: String!): User
+        # Returns the currently authenticated user 
+        me: User
+    }
+
+    type Mutation {
+        # Create new User 
+        addUser(username: String!, email: String!, password: String!): Auth
+        # Authenticate user with email and password for login 
+        login(email: String!, password: String!): Auth
     }
 `;
 
