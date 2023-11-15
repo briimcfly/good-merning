7f80caf0c963ccf75644dabe82d66a38ef1921a5
Bryan Bickel <briimcfly@gmail.com>
2023-11-14 20:49:29 -0800

    fixed an issue on server.js where utils weren't required

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const {ApolloServer} = require('@apollo/server');
 const {expressMiddleware} = require('@apollo/server/express4');
 const path = require('path');
+const {authMiddleware} = require('./utils/auth');
 
 //GraphQL TypeDefs and Resolvers dependencies
 const {typeDefs, resolvers} = require('./schemas');
