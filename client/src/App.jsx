f9b9d852d4da127d068dff6904eda2e711c189de
Bryan Bickel <briimcfly@gmail.com>
2023-11-16 10:59:57 -0800

    users can open login form from landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import { Outlet } from 'react-router-dom'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react'
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <ChakraProvider>
-          <Outlet />
+      <ApolloProvider client={client}>
+        <Outlet />
+      </ApolloProvider>
     </ChakraProvider>
 
   )
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Box, Text, Button } from '@chakra-ui/react';
+import { useState } from 'react';
+import { VStack, Box, Text, Button } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import Login from '../components/Login';
 
 const Landing = () => {
+    //This code will be moved to the Navigation Component once we build it 
+    const [isLoginOpen, setIsLoginOpen] = useState(false);
+    const handleOpenLogin = () => setIsLoginOpen(true);
+    const handleCloseLogin = () => setIsLoginOpen(false);
+
   return (
     <Box textAlign="center" fontSize="xl">
-      <Text mb={4}>This is where the users will land</Text>
-      <Button colorScheme="blue">Go to Listing Page</Button>
+      <Text mb={4} mt={8}>This is where the users will land</Text>
+      <Text mb={4}>We'll update this page as we go, but for now its going to be a router of sorts...</Text>
+      <VStack>
+        <Button colorScheme="blue">Go to Listing Page</Button>
+        <Button colorScheme="green" onClick={handleOpenLogin}>Open Login Component</Button>
+        <Button colorScheme="yellow">Open Signup Component</Button>
+        <Button colorScheme="red" as={Link} to="/1325">Open Error Page</Button>
+      </VStack>
+      <Login isOpen={isLoginOpen} onClose={handleCloseLogin} />
     </Box>
   );
 };
