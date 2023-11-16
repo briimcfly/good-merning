94d9c50d527a2eaba556a5699c6b9801abc9aef3
Nedda Elsayed <nedda_e@yahoo.com>
2023-11-15 21:38:43 -0800

    update chakra

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { ChakraProvider } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
@@ -29,9 +28,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <ChakraProvider>
-      <TheRestOfYourApplication />
-    </ChakraProvider>
     </>
   )
 }
