e3f802aad6b4ba673e7db3c3c42b0fb97a4239e8
Bryan Bickel <briimcfly@gmail.com>
2023-11-17 13:10:32 -0800

    added star component

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Box, Text, Image, Badge, Stack} from '@chakra-ui/react';
+import {Box, Text, Badge, Stack} from '@chakra-ui/react';
 import { formatDate } from '../utils/date';
+import StarRating from './Stars'; 
 
 const ReviewCard = ({review}) => {
     const formattedDate = formatDate(review.postedAt)
@@ -8,9 +9,7 @@ const ReviewCard = ({review}) => {
         <Box p={5} shadow="md" borderWidth="1px">
             <Text fontSize="xl" fontWeight="bold">{review.address}</Text>
             <Stack direction="row" mt={2}>
-                <Badge borderRadius="full" px="2" colorScheme="teal">
-                {review.rating} Stars
-                </Badge>
+                <StarRating rating={review.rating} />
             </Stack>
             <Text mt={2}>{review.comment}</Text>
             <Text mt={2}>Posted by: {review.user.username} on {formattedDate}</Text>
