df8e3318c683d332e50ce29d75d1e91e1dcccc4c
Nozerone <134429390+Nozerone@users.noreply.github.com>
2023-11-16 17:34:53 -0800

    some development on nav component

---
+++ client/src/conponents/Nav/index.jsx
@@ -0,0 +1,40 @@
+import Auth from "../../utils/auth";
+import { Nav } from "react-router-dom";
+
+function Nav() {
+    function showNavigation() {
+        if (Auth.loggedIn()) {
+            return (
+                <Nav>
+                    <div>
+                        <h1>Rental Review</h1>
+                        <input placeholder="Select a new city"></input>
+                        <Button >find</Button>
+                    </div>
+                    <div>
+                        <Button href="/" onClick={() => Auth.logout()}>Logout</Button>
+                    </div>
+
+                </Nav>
+            )
+        } else {
+            return (
+                <nav>
+                <div>
+                    <h1>Rental Review</h1>
+                    <input placeholder="Select a new city"></input>
+                    <Button >find</Button>
+                </div>
+                <div>
+                    <Button href="/login" onClick={() => Auth.logout()}>Login</Button>
+                    <Button href="/signup">Sign up</Button>
+                </div>
+
+            </nav>
+            )
+        }
+    }
+
+}
+
+export default Nav;
