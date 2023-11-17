c01d2205367a689795a7a8a0d3a0185c813c3c96
Bryan Bickel <briimcfly@gmail.com>
2023-11-17 13:23:41 -0800

    review card styling update

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Box, Text, Badge, Stack} from '@chakra-ui/react';
+import {Box, Text, Stack} from '@chakra-ui/react';
+import { FaUserCircle } from "react-icons/fa";
 import { formatDate } from '../utils/date';
 import StarRating from './Stars'; 
 
@@ -7,12 +8,15 @@ const ReviewCard = ({review}) => {
     const formattedDate = formatDate(review.postedAt)
     return (
         <Box p={5} shadow="md" borderWidth="1px">
-            <Text fontSize="xl" fontWeight="bold">{review.address}</Text>
+            <Stack direction="row" mt={2}>
+            <FaUserCircle size={30}/>
+            <Text fontSize="xl" fontWeight="bold">{review.user.username}</Text>
+            </Stack>
             <Stack direction="row" mt={2}>
                 <StarRating rating={review.rating} />
             </Stack>
             <Text mt={2}>{review.comment}</Text>
-            <Text mt={2}>Posted by: {review.user.username} on {formattedDate}</Text>
+            <Text mt={2}>Posted on {formattedDate}</Text>
         </Box> 
     )
 }
