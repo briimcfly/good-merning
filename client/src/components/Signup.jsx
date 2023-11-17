66781fa4c0626b78c237b4151e7953ea0e8819bc
Rael Wanjala <raelnwanjala@gmail.com>
2023-11-16 19:10:05 -0800

    Added handlechange function for form state updates

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -4,9 +4,20 @@ import {ADD_USER} from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const Signup = ({isOpen, onClose}) => {
+    //state to manage form data and errors
     const [formState, setFormState] = useState({
         username: '',
         email: '',
         password: '',
     });
     const [addUser, {error}] = useMutation(ADD_USER);
+
+    //handle input changes
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormState({
+            ...formState, [name]: value,
+        });
+    }
+
+    
