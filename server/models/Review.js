ddbf8d69ea9ad5a5a40505151569882c7da45674
Bryan Bickel <briimcfly@gmail.com>
2023-11-17 23:08:51 -0800

    updated review model to add more ratings

--- server/models/Review.js
+++ server/models/Review.js
@@ -28,6 +28,79 @@ const reviewSchema = new Schema({
         min: 1,
         max: 5
     },
+    landLordScore: {
+        responsiveness: {
+            type: Number,
+            min: 1,
+            max: 5
+        },
+        attitude: {
+            type: Number,
+            min: 1,
+            max: 5
+        },
+        maintenance: {
+            type: Number,
+            min: 1,
+            max: 5
+        },
+        leaseManagement: {
+            type: Number,
+            min: 1,
+            max: 5
+        }
+    },
+    propertyScore: {
+        condition: {
+            type: Number,
+            min: 1,
+            max: 5
+        },
+        amenities: {
+            type: Number,
+            min: 1,
+            max: 5
+        },
+        safety: {
+            type: Number,
+            min: 1,
+            max: 5
+        }
+    },
+    areaScore: {
+        location: {
+            type: Number,
+            min: 1,
+            max: 5
+        },
+        noiseLevel: {
+            type: Number,
+            min: 1,
+            max: 5
+        },
+        neighborhood: {
+            type: Number,
+            min: 1,
+            max: 5
+        },
+    },
+    financialAspects: {
+        rentFairness: {
+            type: Number,
+            min: 1,
+            max: 5
+        },
+        rentIncreases: {
+            type: Number,
+            min: 1,
+            max: 5
+        },
+        value: {
+            type: Number,
+            min: 1,
+            max: 5
+        },
+    },
     images: {
         type: [String],
         default: []
@@ -39,6 +112,6 @@ const reviewSchema = new Schema({
 });
 
 //Create the Listing model using schema 
-const Listing = model('Review', reviewSchema);
+const Review = model('Review', reviewSchema);
 
-module.exports = Listing;
+module.exports = Review;
