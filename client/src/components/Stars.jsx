e9bf6550d979d1cb605942c4ad614334f870889c
Bryan Bickel <briimcfly@gmail.com>
2023-11-19 16:11:51 -0800

    updated star styling

--- client/src/components/Stars.jsx
+++ client/src/components/Stars.jsx
@@ -14,20 +14,37 @@ const getStarIcon = (rating,index) => {
     }
 }
 
-const StarRating = ({rating})=> {
+const StarRating = ({rating}) => {
     return (
         <Box display="flex">
-            {[...Array(5)].map((_, index) => (
-                <Icon
-                as={getStarIcon(rating,index)}
-                key={index}
-                color = {index < rating ? 'teal.500' : 'gray.300'}
-                w={6}
-                h={6}
-                />
-            ))}
+            {[...Array(5)].map((_, index) => {
+                // Determine the color based on the rating
+                let color;
+                if (rating <= 2) {
+                    // If rating is 2 or less, color the stars red
+                    color = index < rating ? 'red.300' : 'gray.300';
+                } 
+                else if (rating <= 3 && rating > 2){
+                    color = index < rating ? 'yellow.400' : 'gray.300';
+                }
+                else {
+                    // If rating is more than 2, color the stars with the original color
+                    color = index < rating ? 'teal.500' : 'gray.300';
+                }
+                
+                return (
+                    <Icon
+                        as={getStarIcon(rating, index)}
+                        key={index}
+                        color={color}
+                        w={6}
+                        h={6}
+                    />
+                );
+            })}
         </Box>
-    )
-}
+    );
+};
+
 
 export default StarRating;
