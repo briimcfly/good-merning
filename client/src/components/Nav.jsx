a7dd7a8ab929405bf6b4f774f6b6051349fdf218
Nedda Elsayed <nedda_e@yahoo.com>
2023-11-21 23:14:08 -0800

    Created new branch due to merge issues - Applied all the changes I had made in the previous without conflict. Fixed location of logo and search bar in Nav, added button to CityRentals, created modal for new reviews, still figuring out Legend.

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,81 +1,129 @@
-import React from 'react';
-import { useState } from 'react';
-import { ArrowRightIcon, ArrowUpIcon } from '@chakra-ui/icons';
-import {  Flex, Button, Heading, Spacer, HStack, Box, Input } from "@chakra-ui/react"
-import {Link} from 'react-router-dom';
-import Auth from '../utils/auth';
-import Login from './Login';
-import Signup from './Signup';
+import React from "react";
+import { useState } from "react";
+import { ArrowRightIcon, ArrowUpIcon } from "@chakra-ui/icons";
+import {
+	Flex,
+	Button,
+	Heading,
+	Spacer,
+	HStack,
+	Box,
+	Input,
+	Stack,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import Auth from "../utils/auth";
+import Login from "./Login";
+import Signup from "./Signup";
+import CitySearchNav from "./CitySearchNav";
 
 const Nav = () => {
-  const [isLoginOpen, setIsLoginOpen] = useState(false);
-  //Test this line
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isSignupOpen, setIsSignupOpen] = useState(false);
+	const [isLoginOpen, setIsLoginOpen] = useState(false);
+	//Test this line
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [isSignupOpen, setIsSignupOpen] = useState(false);
 
-  const handleOpenLogin = () => setIsLoginOpen(true);
-  const handleCloseLogin = () => setIsLoginOpen(false);
+	const handleOpenLogin = () => setIsLoginOpen(true);
+	const handleCloseLogin = () => setIsLoginOpen(false);
 
-  const handleLogin = () => {
-    isLoggedIn()
-    // If login is successful, set isLoggedIn to true
-    setIsLoggedIn(true);
-  };
-  const handleOpenSignup = () => setIsSignupOpen(true); 
-  const handleCloseSignup = () => setIsSignupOpen(false);
+	const handleLogin = () => {
+		isLoggedIn();
+		// If login is successful, set isLoggedIn to true
+		setIsLoggedIn(true);
+	};
+	const handleOpenSignup = () => setIsSignupOpen(true);
+	const handleCloseSignup = () => setIsSignupOpen(false);
 
-  const handleSignup = () => {
-    isLoggedIn()
-    // If signup is successful, set isLoggedIn to true
-    setIsLoggedIn(true);
-    // If signup is successful, close the signup modal
-    handleCloseSignup();
-  };
+	const handleSignup = () => {
+		isLoggedIn();
+		// If signup is successful, set isLoggedIn to true
+		setIsLoggedIn(true);
+		// If signup is successful, close the signup modal
+		handleCloseSignup();
+	};
 
-  return (
-
-    //if Not Logged In
-    <Flex as="nav" width="100vw" height="64px" top="20px" left="20px" justify="space-between" pt={10} pb={10} pl={6} pr={6} alignItems="center" >
-      <Box gap="32px">
-
-      {/* Dwellex Logo */}
-      <Link to = '/' style ={{textDecoration: 'none'}}> 
-        <Heading as="h1" fontWeight="700" size="xl" lineHeight="28px" >
-          Dwellex
-        </Heading>
-      </Link>
-      <Box>
-        <Input placeholder="enter a new city" />
-        <Button>Find</Button>
-      </Box>
-
-      </Box>
-      <Spacer />
-      <HStack>
-        {Auth.loggedIn() ? (
-          <>
-          <Button onClick={() => Auth.logout()} colorScheme="blue"
-                  variant="solid"
-                  type="submit"
-                  padding="0px, 12px, 0px, 12px"
-                  gap="8px">Logout</Button>
-          </>
-        ) : (
-          <>
-          <Button border="1px" leftIcon={ < ArrowRightIcon /> } variant="outline" color="blue.500" onClick={handleOpenLogin}>Login</Button>
-           <Button leftIcon={ <ArrowUpIcon/> } colorScheme="blue"
-                  variant="solid"
-                  type="submit"
-                  padding="0px, 12px, 0px, 12px"
-                  gap="8px"
-                  onClick={handleOpenSignup}>Sign Up</Button>
-         </>
-       )}
-     </HStack>
-     <Login isOpen={isLoginOpen} onClose={handleCloseLogin} onLogin={handleLogin} />
-    {isSignupOpen && <Signup isOpen={isSignupOpen} onClose={handleCloseSignup} onSignup={handleSignup} />}
-   </Flex>
- );
+	return (
+		//if Not Logged In
+		<Flex
+			as="nav"
+			width="100vw"
+			height="64px"
+			top="20px"
+			left="20px"
+			justify="space-between"
+			pt={10}
+			pb={10}
+			pl={6}
+			pr={6}
+			alignItems="center"
+		>
+			<Box gap="32px">
+				{/* Dwellex Logo */}
+				<Stack direction="row" spacing={4}>
+					<Link to="/" style={{ textDecoration: "none" }}>
+						<Heading as="h1" fontWeight="700" size="xl" lineHeight="28px">
+							Dwellex
+						</Heading>
+					</Link>
+					<Box>
+						<CitySearchNav />
+					</Box>
+				</Stack>
+			</Box>
+			<Spacer />
+			<HStack>
+				{Auth.loggedIn() ? (
+					<>
+						<Button
+							onClick={() => Auth.logout()}
+							colorScheme="blue"
+							variant="solid"
+							type="submit"
+							padding="0px, 12px, 0px, 12px"
+							gap="8px"
+						>
+							Logout
+						</Button>
+					</>
+				) : (
+					<>
+						<Button
+							border="1px"
+							leftIcon={<ArrowRightIcon />}
+							variant="outline"
+							color="blue.500"
+							onClick={handleOpenLogin}
+						>
+							Login
+						</Button>
+						<Button
+							leftIcon={<ArrowUpIcon />}
+							colorScheme="blue"
+							variant="solid"
+							type="submit"
+							padding="0px, 12px, 0px, 12px"
+							gap="8px"
+							onClick={handleOpenSignup}
+						>
+							Sign Up
+						</Button>
+					</>
+				)}
+			</HStack>
+			<Login
+				isOpen={isLoginOpen}
+				onClose={handleCloseLogin}
+				onLogin={handleLogin}
+			/>
+			{isSignupOpen && (
+				<Signup
+					isOpen={isSignupOpen}
+					onClose={handleCloseSignup}
+					onSignup={handleSignup}
+				/>
+			)}
+		</Flex>
+	);
 };
 
 export default Nav;
