e74721797a8fa933529044a54fc4eb1a0de4a868
Bryan Bickel <briimcfly@gmail.com>
2023-11-24 11:28:58 -0800

    Fixed header responsive issues: resolves #59 , resolves #36 , resolves #27

--- client/src/components/molecules/PageHeader.jsx
+++ client/src/components/molecules/PageHeader.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
-import {Flex, Heading, Button} from '@chakra-ui/react';
+import {Flex, Stack, Button, Heading} from '@chakra-ui/react';
 import { FaPlusSquare } from 'react-icons/fa';
 
 const PageHeader = ({city, state, address, titlePrefix}) => {
     const displayText = address ? address: `${city}, ${state}`;
-    const title = titlePrefix ? 
-    `${titlePrefix} \n ${displayText}` :
-    `Listings in \n ${displayText}`;
+    //Split Header Text into two parts 
+    const titleFirstLine = titlePrefix || 'Listings in ';
+    const titleSecondLine = displayText
 
     return (
-        <Flex justifyContent='space-between' p={2}>
-            <Heading as='h1' mb={8}>
-                {title} 
-            </Heading>
-            <Button leftIcon={<FaPlusSquare/>}colorScheme="teal" to="/add-review" mt={1}>
+        <Flex
+        direction={{ base: 'column', md: 'row' }} 
+        justifyContent="space-between"
+        alignItems={{ base: 'start', md: 'center' }} 
+        p={2}
+        wrap="wrap"
+         >
+            <Stack mb={{ base: 4, md: 8 }} spacing="3">
+                <Heading textColor ='gray.600' as="h3" size={{ base: 'xs', sm: 'sm', md: 'md' }}>
+                    {titleFirstLine}
+                </Heading>
+                <Heading as="h2" size={{ base: 'md', sm: 'xl', md: '2xl' }}>
+                    {titleSecondLine}
+                </Heading>
+            </Stack>
+            <Button leftIcon={<FaPlusSquare/>}colorScheme="teal" to="/add-review" mt={{ base: 4, md: 0 }} >
                 Add Review
             </Button>
         </Flex>
