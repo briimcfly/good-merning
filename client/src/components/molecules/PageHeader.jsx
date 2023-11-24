6d27dd8726fe67c1b7335856d23d69956f57ba31
Bryan Bickel <briimcfly@gmail.com>
2023-11-24 11:28:58 -0800

    Fixed header responsive issues: resolves #59 , resolves #36 , resolves #27

--- client/src/components/molecules/PageHeader.jsx
+++ client/src/components/molecules/PageHeader.jsx
@@ -61,37 +61,3 @@ const PageHeader = ({city, state, address, titlePrefix}) => {
 }
 
 export default PageHeader;
-
-//import React from 'react';
-
-
-// const PageHeader = ({city, state, address, titlePrefix}) => {
-//     const displayText = address ? address: `${city}, ${state}`;
-//     //Split Header Text into two parts 
-//     const titleFirstLine = titlePrefix || 'Listings in ';
-//     const titleSecondLine = displayText
-
-//     return (
-//         <Flex
-//         direction={{ base: 'column', md: 'row' }} 
-//         justifyContent="space-between"
-//         alignItems={{ base: 'start', md: 'center' }} 
-//         p={2}
-//         wrap="wrap"
-//          >
-//             <Stack mb={{ base: 4, md: 8 }} spacing="3">
-//                 <Heading textColor ='gray.600' as="h3" size={{ base: 'xs', sm: 'sm', md: 'md' }}>
-//                     {titleFirstLine}
-//                 </Heading>
-//                 <Heading as="h2" size={{ base: 'md', sm: 'xl', md: '2xl' }}>
-//                     {titleSecondLine}
-//                 </Heading>
-//             </Stack>
-//             <Button leftIcon={<FaPlusSquare/>}colorScheme="teal" to="/add-review" mt={{ base: 4, md: 0 }} >
-//                 Add Review
-//             </Button>
-//         </Flex>
-//     )
-// }
-
-// export default PageHeader;
